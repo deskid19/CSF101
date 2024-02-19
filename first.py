48d72d52ffd0502572e1f9df80f6c77a421b73ae
unknown <02230107.cst@rub.edu.bt>
2024-02-19 07:23:05 -0800

    first commit

--- first.py
+++ first.py
@@ -1,1 +1,15 @@
 print("hello world")
+print("hey!")
+print("i love programming")
+grade = 95
+if grade >= 90:
+    print("Grade: A")
+elif grade >= 80:
+    print("Grade: B")
+elif grade >= 70:
+    print("Grade: C")
+elif grade >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
+print("I am as cool as my programe ECE")
