d8cc6145c9a3906cb8ed02f7f1fbf4bbb66e5263
unknown <02230107.cst@rub.edu.bt>
2024-03-04 01:46:36 -0800

    python

---
+++ python.py
@@ -0,0 +1,10 @@
+name = input("Enter your name:")
+num_1 = float(input("Enter first number:"))
+num_2 = float(input("Enter second number:"))
+
+print(num_1 + num_2)
+print(num_1 - num_2)
+print(num_1 * num_2)
+print(num_1 % num_2)
+
+print(f"hello {name}")
